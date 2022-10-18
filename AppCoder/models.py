b4495c23577d5dfccbfab19a6fe69d41713e61ac
Nacho Eche <echeverriaign@gmail.com>
2022-10-18 16:45:52 -0300

    Comentario

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -1,3 +1,8 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=250)
+    apellido = models.CharField(max_length=500)
