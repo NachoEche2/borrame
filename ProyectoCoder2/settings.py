59eb20750ed7c0d4de058ee5e37824b02ce7d9fc
Nacho Eche <echeverriaign@gmail.com>
2022-10-18 15:13:35 -0300

    AGREGO TEMPLATES

--- ProyectoCoder2/settings.py
+++ ProyectoCoder2/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'ProyectoCoder2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["C:\Users\nacho\Desktop\Coder House\Python\django\ProyectoCoder2\AppCoder\templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
